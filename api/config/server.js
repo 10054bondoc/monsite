014b9c18d5429552836b1f8ad7a1d0eddb62bb16
Ian <10054bondoc@gmail.com>
2021-10-04 11:27:45 +0800

    initials

---
+++ api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3ab8b4fb6525e110f5fb5689b398757f'),
+    },
+  },
+});
---
+++ api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'ccaa11af-5ea5-4a7f-ac71-84405ab59ced'
+};
